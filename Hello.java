2246a1c3b6880bae28a5dc4bd3f33de88db02153
shivaconceptsolution <shivaconceptsoltion@gmail.com>
2025-10-13 11:17:56 +0530

    new changes on Hello.java file

--- Hello.java
+++ Hello.java
@@ -4,6 +4,8 @@ class Hello
     public static void main(String args[])
     {
          System.out.println("Welcome in DEVOPS CI-CD PIPELINE");
+         System.out.println(This is Jenkins WebHooks Integration");
+
      
     }  
 
